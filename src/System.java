d193389fbb7a5a7917799f189f6832a690ec7f93
olga <olwi1000@stud.ek.dk>
2026-04-16 13:43:09 +0200

    :)

--- src/System.java
+++ src/System.java
@@ -1,2 +1,58 @@
+import util.TextUI;
+
+import java.util.ArrayList;
+
 public class System {
+    private User currentUser;
+    private MediaLibrary mediaLibrary;
+    private Menu menu;
+    private ArrayList<User> allUsers;
+    TextUI ui;
+
+    void createExistingUser (String data){
+        String [] values= data.split(",");
+        User u = new User(values[0].trim(),values[1].trim());
+
+        if (values.length>2){ //hvis der er mere end username og password gemt på en bruger
+            // skal disse tildeles hver deres arrayList watched eller wantToWatch. eller vi kan splitte på et andet symbol også
+            //først skabe to string arrays...
+
+        }
+
+        allUsers.add(u);
+    }
+
+    void createAllUsers(ArrayList<String> data){ //igen hvad gør vi med arraylister?
+        for (String s:data){
+            String [] values= s.split(",");
+            User u= new User(values[0].trim(), values[1].trim());
+            allUsers.add(u);
+        }
+
+    }
+
+    void createNewUser(){
+        String userName=ui.promptText("Input username");
+        String passWord=ui.promptText("input password");
+
+        User u= new User(userName,passWord);
+        allUsers.add(u);
+
+    }
+
+    void createNewUser2 (String name, String password){
+        User u=new User(name, password);
+        allUsers.add(u);
+    }
+
+
+
+
 }
+
+//class System {
+//- User currentUser
+//- MediaLibrary mediaLibrary
+//- Menu menu
+//- ArrayList<User> allUsers
+//}
--- src/User.java
+++ src/User.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class User {
+    private String userName;
+    private String passWord;
+    private ArrayList <Media> wantToWatch;
+    private ArrayList <Media> watched;
+
+    public User (String userName, String passWord){
+        this.userName=userName;
+        this.passWord=passWord;
+        wantToWatch=new ArrayList<>();
+        watched= new ArrayList<>();
+    }
+
+    public User (String userName, String passWord, ArrayList<Media> wantToWatch, ArrayList<Media> watched){
+        this.userName=userName;
+        this.passWord=passWord;
+        this.wantToWatch=wantToWatch;
+        this.watched=watched;
+    }
+
+
+    public void addMediaToList(Media m, ArrayList<Media> list){
+        list.add(m);
+    }
+
+
+
 }
+
+
+
+//class User{
+//- String username
+//- String password
+//- ArrayList<Media> wantToWatch
+//- ArrayList<Media> watched
+//+ void addMediaToList(Media, ArrayList<Media>)
+//}
