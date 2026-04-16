446dab2e909062d5b2e129de37570e67eb4daa48
Ledut-dev <lued1000@stud.ek.dk>
2026-04-16 11:49:42 +0200

    Sp3 - Classdiagram, FileIO and TextUI, updated domain model

---
+++ src/util/FileIO.java
@@ -0,0 +1,49 @@
+package util;/*
+*  Utility class to persist the Strings in an ArrayList
+*  To use, each object in a list must first be serialized (field values of the object are added to the same String, separated by comma)
+*
+*  TODO: To avoid leakage, both scanner and FileWriter must be closed, suggestion to use try-with-ressources in both methods
+*  TODO: catch exception if the file is found but empty
+* */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileIO {
+
+
+    public void saveData(ArrayList<String> list, String path){
+        try {
+
+            FileWriter writer = new FileWriter(path);
+            for (String s : list) {
+                writer.write(s+"\n");
+            }
+            writer.close();
+
+        }catch (IOException e) {
+            System.out.println("problem: "+ e.getMessage());
+        }
+    }
+
+    public static ArrayList<String> readData(String path) {
+        ArrayList<String> data = new ArrayList<>();
+        File file = new File(path);
+        try {
+            Scanner scan = new Scanner(file);
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                data.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Filen findes ikke");
+        }
+        return data;
+    }
+
+}
